d993a8ea9d3e27e543a8c5dcb8f64b932693c31b
Lucas Valli Azevedo <lucasvalliazevedo@hotmail.com>
2024-06-20 22:41:04 -0300

    atualização 20/06/2024

--- app/src/main/java/br/com/lucas/valli/fluxodecaixa/TelaSobre.java
+++ app/src/main/java/br/com/lucas/valli/fluxodecaixa/TelaSobre.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,6 +29,7 @@ public class TelaSobre extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        DadosAtualizacaoApp();
 
     }
 
@@ -42,50 +45,70 @@ public class TelaSobre extends AppCompatActivity {
             }
         });
 
-        binding.btnAtualizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openPlayStore();
-            }
-        });
+
     }
 
-    private void openPlayStore() {
-        // Substitua "seu_pacote" pelo pacote do seu aplicativo na Play Store
-        String appPackageName = "br.com.lucas.valli.fluxodecaixa";
-        try {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-        } catch (android.content.ActivityNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-        }
-    }
 
-    public void DadosAtualizacaoApp(){
-
+    public void DadosAtualizacaoApp() {
         DocumentReference documentReference = db.collection("ArquivosDoApp").document("AppVersion");
 
         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> TaskAppVersion) {
-
-                if (TaskAppVersion.isSuccessful()){
+                if (TaskAppVersion.isSuccessful()) {
                     DocumentSnapshot AppVersion = TaskAppVersion.getResult();
 
-                    if (AppVersion.exists() && AppVersion.exists()){
-                        binding.btnAtualizar.setVisibility(View.VISIBLE);
-                        binding.txtDonwloadDisponivel.setVisibility(View.VISIBLE);
+                    if (AppVersion != null && AppVersion.contains("Versao")) {
+                        String appVersao = AppVersion.getString("Versao");
+                        String versaoInstalada = getInstalledVersion();
 
-                        String link = AppVersion.getString("Link");
-                        String versao = AppVersion.getString("Versao");
+                        if (novaVersaoDisponivel(versaoInstalada, appVersao)) {
+                            binding.btnAtualizar.setVisibility(View.VISIBLE);
+                            binding.txtDonwloadDisponivel.setVisibility(View.VISIBLE);
 
-                        binding.txtDonwloadDisponivel.setText("Versão "+versao+" disponível para download " + "\n\n" + "BETA" );
+                            binding.txtDonwloadDisponivel.setText("Versão " + appVersao + " disponível para download ");
+                            binding.btnAtualizar.setVisibility(View.VISIBLE);
 
-                    }else {
+                            binding.btnAtualizar.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    String appPackageName = "br.com.lucas.valli.fluxodecaixa";
+                                    String playStoreUrl = "market://details?id=" + appPackageName;
+                                    String fallbackUrl = "https://play.google.com/store/apps/details?id=" + appPackageName;
+
+                                    try {
+                                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(playStoreUrl));
+                                        intent.setPackage("com.android.vending");
+                                        startActivity(intent);
+                                    } catch (android.content.ActivityNotFoundException e) {
+                                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(fallbackUrl));
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        }
+                    } else {
                         Log.i("TESTEFIREBASE", "NÃO HÁ DADOS");
                     }
                 }
-
             }
         });
     }
+
+    private String getInstalledVersion() {
+        String versaoInstalada = "";
+        try {
+            PackageManager pm = getPackageManager();
+            PackageInfo pInfo = pm.getPackageInfo(getPackageName(), 0);
+            versaoInstalada = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versaoInstalada;
+    }
+
+    private boolean novaVersaoDisponivel(String installedVersion, String latestVersion) {
+        return !installedVersion.equals(latestVersion);
+    }
+
 }
